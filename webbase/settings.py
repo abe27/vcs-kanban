37239be4d4a6f8069c094d4a2dea149b7086b205
Taweechai Yuenyang <krumii.it@gmail.com>
2023-08-09 08:03:45 +0700

    disable bootstrap

--- webbase/settings.py
+++ webbase/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bootstrap5',
+    # 'bootstrap5',
     'vcs_kanban.apps.VcsKanbanConfig',
 ]
 
