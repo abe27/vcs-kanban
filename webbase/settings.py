e1de0637a9d43a8561cd2977d09f838353abf1d4
Taweechai Yuenyang <krumii.it@gmail.com>
2023-08-09 08:03:12 +0700

    update color

--- webbase/settings.py
+++ webbase/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     # "admin_interface",
-    "colorfield",
+    # "colorfield",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
